64b9f886e3d5df85ce86378af11ac652d30296df
Ryan Davis <ryancraigdavis@github.com>
2024-03-27 23:21:52 -0700

    #4 Added base forex code

---
+++ src/quart_example/app.py
@@ -0,0 +1,36 @@
+"""This app does foreign exchange requests"""
+from quart import Quart, jsonify, request
+
+app = Quart(__name__)
+
+@app.get("/currencies")
+async def currencies_supported():
+    currencies = currency_list()
+    return jsonify(currencies)
+
+def currency_list():
+    """List of currencies supported"""
+    currency_list = {"dollar": "USD", "euro": "EUR", "british sterling": "GBP"}
+    return currency_list
+
+@app.post("/currency_supported")
+async def currency_supported():
+    """Tests if the inputted currency is supported"""
+    input_currency = await request.get_json()
+    currencies = currency_list()
+    if currencies.get(input_currency["currency_name"]):
+        return "Currency is accepted"
+    else:
+        return "Currency not supported"
+
+@app.post("/convert_currency")
+async def convert_currency():
+    data = await request.get_json()
+    return {"input": data, "extra": True}
+
+
+def run() -> None:
+    app.run()
+
+if __name__ == "__main__":
+    run()
