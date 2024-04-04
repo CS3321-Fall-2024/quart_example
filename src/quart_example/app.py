6195f6eddfb78ef5cd7cc9ff99e83415eaf21345
Ryan Davis <ryancraigdavis@github.com>
2024-04-04 12:06:52 -0700

    #4 env added

--- src/quart_example/app.py
+++ src/quart_example/app.py
@@ -1,17 +1,26 @@
 """This app does foreign exchange requests"""
+
 from quart import Quart, jsonify, request
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
 
 app = Quart(__name__)
+
 
 @app.get("/currencies")
 async def currencies_supported():
     currencies = currency_list()
     return jsonify(currencies)
 
+
 def currency_list():
     """List of currencies supported"""
     currency_list = {"dollar": "USD", "euro": "EUR", "british sterling": "GBP"}
     return currency_list
+
 
 @app.post("/currency_supported")
 async def currency_supported():
@@ -23,8 +32,10 @@ async def currency_supported():
     else:
         return "Currency not supported"
 
+
 def run() -> None:
     app.run()
+
 
 if __name__ == "__main__":
     run()
